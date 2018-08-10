caa2a4226ef3f335c75ee34fcccc2c9ae69ad276
Gal PC <glpkmtg@gmail.com>
2018-08-10 19:34:51 +0300

    Poster on click to toggle pause

---
+++ src/main/scala/common/SwingUtils.scala
@@ -0,0 +1,40 @@
+package common
+
+import java.awt.event.{MouseEvent, MouseListener}
+import java.awt.image.BufferedImage
+import java.awt.{Image, RenderingHints}
+
+import javax.swing.{ImageIcon, JLabel}
+
+import scala.swing.Component
+
+object SwingUtils {
+  implicit class RichComponent($: Component) {
+    // Because reactions is BS and doesn't work.
+    def onMouseClick(f: => Any): Component = {
+      $.peer.addMouseListener(new MouseListener {
+        override def mouseExited(e: MouseEvent): Unit = ()
+        override def mousePressed(e: MouseEvent): Unit = ()
+        override def mouseReleased(e: MouseEvent): Unit = ()
+        override def mouseEntered(e: MouseEvent): Unit = ()
+        override def mouseClicked(e: MouseEvent): Unit = f
+      })
+      $
+    }
+  }
+  implicit class RichImage(image: Image) {
+    def toSquareImageIcon(side: Int): ImageIcon = toImageIcon(side, side)
+    def toImageIcon(height: Int, width: Int): ImageIcon = {
+      val bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
+      val graphics = bufferedImage.createGraphics
+      graphics.setRenderingHint(
+        RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR)
+      graphics.drawImage(image, 0, 0, width, height, null)
+      graphics.dispose()
+      new ImageIcon(bufferedImage)
+    }
+  }
+  implicit class RichImageIcon($: ImageIcon) {
+    def toComponent: Component = Component wrap new JLabel($)
+  }
+}
--- src/main/scala/ui/PosterComponent.scala
+++ src/main/scala/ui/PosterComponent.scala
@@ -1,40 +1,27 @@
 package ui
 
-import java.awt.RenderingHints
-import java.awt.image.BufferedImage
-
 import comm.PosterComm
 import common.RichTask._
+import common.SwingUtils._
 import common.rich.func.{MoreObservableInstances, ToMoreMonadPlusOps}
 import javax.inject.Inject
-import javax.swing.ImageIcon
 import player.{Player, SongChanged}
 
-import scala.swing.{Dimension, Image, Label}
+import scala.swing.{Dimension, Label}
 
 class PosterComponent @Inject()(player: Player, posterComm: PosterComm) extends Label
     with ToMoreMonadPlusOps with MoreObservableInstances {
-  private val playerEvents = player.events
   private val SideLengthInPixels = 500
   preferredSize = new Dimension(SideLengthInPixels, SideLengthInPixels)
 
-  private def setImage(image: Image): Unit = {
-    val bi = new BufferedImage(SideLengthInPixels, SideLengthInPixels, BufferedImage.TYPE_INT_ARGB)
-    val g2 = bi.createGraphics
-
-    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR)
-    g2.drawImage(image, 0, 0, SideLengthInPixels, SideLengthInPixels, null)
-    g2.dispose()
-
-    icon = new ImageIcon(bi)
-  }
-
-  playerEvents
+  player.events
       .observeOn(IOPool.scheduler)
       .select[SongChanged]
       .map(_.newSong)
       .flatMap(posterComm.poster(_).toObservable(IOPool()))
       .observeOn(SwingEdtScheduler())
-      .doOnNext(setImage)
+      .doOnNext(e => icon = e.toSquareImageIcon(SideLengthInPixels))
       .subscribe()
+
+  this.onMouseClick(player.togglePause.fireAndForget())
 }
